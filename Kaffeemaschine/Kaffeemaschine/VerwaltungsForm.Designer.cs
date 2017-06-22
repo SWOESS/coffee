e7b9c49fe339e545fb33bcb35b4b54af19d5969c
Björn Brüstle <djherodj@gmx.at>
2017-06-22 11:19:22 +0200

    pfusch

--- Kaffeemaschine/Kaffeemaschine/VerwaltungsForm.Designer.cs
+++ Kaffeemaschine/Kaffeemaschine/VerwaltungsForm.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
             this.ingredients = new System.Windows.Forms.DataVisualization.Charting.Chart();
@@ -69,6 +70,7 @@
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 20;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
@@ -91,9 +93,9 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(803, 548);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1606, 1054);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // button1
@@ -106,10 +108,11 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.button1.Location = new System.Drawing.Point(643, 489);
+            this.button1.Location = new System.Drawing.Point(1286, 942);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button1.Name = "button1";
             this.tableLayoutPanel1.SetRowSpan(this.button1, 2);
-            this.button1.Size = new System.Drawing.Size(157, 56);
+            this.button1.Size = new System.Drawing.Size(314, 106);
             this.button1.TabIndex = 4;
             this.button1.Text = "UserInterface";
             this.button1.UseVisualStyleBackColor = false;
@@ -120,16 +123,22 @@
             this.ingredients.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.BorderColor = System.Drawing.Color.Yellow;
+            chartArea1.CursorY.LineColor = System.Drawing.Color.Black;
             chartArea1.Name = "ChartArea1";
             this.ingredients.ChartAreas.Add(chartArea1);
             this.tableLayoutPanel1.SetColumnSpan(this.ingredients, 15);
             legend1.Name = "Legend1";
             this.ingredients.Legends.Add(legend1);
-            this.ingredients.Location = new System.Drawing.Point(42, 29);
-            this.ingredients.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ingredients.Location = new System.Drawing.Point(84, 56);
+            this.ingredients.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ingredients.Name = "ingredients";
             this.tableLayoutPanel1.SetRowSpan(this.ingredients, 15);
-            this.ingredients.Size = new System.Drawing.Size(596, 401);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.ingredients.Series.Add(series1);
+            this.ingredients.Size = new System.Drawing.Size(1192, 772);
             this.ingredients.TabIndex = 5;
             this.ingredients.Text = "Füllstände";
             this.ingredients.Click += new System.EventHandler(this.chart1_Click);
@@ -140,10 +149,11 @@
             // 
             // VerwaltungsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(803, 548);
+            this.ClientSize = new System.Drawing.Size(1606, 1054);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "VerwaltungsForm";
             this.Text = "VerwaltungsForm";
             this.tableLayoutPanel1.ResumeLayout(false);
