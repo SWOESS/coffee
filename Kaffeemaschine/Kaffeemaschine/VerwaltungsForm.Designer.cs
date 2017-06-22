5f378d870bef85c72d1372244469cec1bb09b0d8
Björn Brüstle <djherodj@gmx.at>
2017-06-22 10:00:17 +0200

    dsgs

--- Kaffeemaschine/Kaffeemaschine/VerwaltungsForm.Designer.cs
+++ Kaffeemaschine/Kaffeemaschine/VerwaltungsForm.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.ingredients = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ingredients)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -59,11 +67,11 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
             this.tableLayoutPanel1.Controls.Add(this.button1, 16, 18);
-            this.tableLayoutPanel1.Controls.Add(this.progressBar1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ingredients, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 20;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
@@ -90,7 +98,6 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1606, 1237);
             this.tableLayoutPanel1.TabIndex = 1;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // button1
             // 
@@ -103,7 +110,7 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.button1.Location = new System.Drawing.Point(1286, 1104);
-            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
             this.button1.Name = "button1";
             this.tableLayoutPanel1.SetRowSpan(this.button1, 2);
             this.button1.Size = new System.Drawing.Size(314, 127);
@@ -112,12 +119,37 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // progressBar1
+            // errorProvider1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(83, 64);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(74, 55);
-            this.progressBar1.TabIndex = 5;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ingredients
+            // 
+            this.ingredients.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.ingredients.ChartAreas.Add(chartArea1);
+            this.tableLayoutPanel1.SetColumnSpan(this.ingredients, 15);
+            legend1.Name = "Legend1";
+            this.ingredients.Legends.Add(legend1);
+            this.ingredients.Location = new System.Drawing.Point(83, 64);
+            this.ingredients.Name = "ingredients";
+            this.ingredients.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            this.tableLayoutPanel1.SetRowSpan(this.ingredients, 15);
+            series1.ChartArea = "ChartArea1";
+            series1.LabelBorderWidth = 2;
+            series1.Legend = "Legend1";
+            series1.Name = "Füllstand";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Verkauf";
+            this.ingredients.Series.Add(series1);
+            this.ingredients.Series.Add(series2);
+            this.ingredients.Size = new System.Drawing.Size(1194, 909);
+            this.ingredients.TabIndex = 5;
+            this.ingredients.Text = "ingredients";
+            this.ingredients.Click += new System.EventHandler(this.chart1_Click);
             // 
             // VerwaltungsForm
             // 
@@ -125,11 +157,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1606, 1237);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "VerwaltungsForm";
             this.Text = "VerwaltungsForm";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.VerwaltungsForm_FormClosing);
             this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ingredients)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -138,6 +171,7 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ingredients;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- Kaffeemaschine/Kaffeemaschine/VerwaltungsForm.cs
+++ Kaffeemaschine/Kaffeemaschine/VerwaltungsForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Kaffeemaschine
 {
@@ -24,6 +25,26 @@ namespace Kaffeemaschine
             frm.StartPosition = FormStartPosition.Manual;
             frm.Show();
             this.Hide();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            // Data arrays.
+            string[] seriesArray = { "Kaffe", "Tee", "Kakao", "Zucker" };
+            int[] pointsArray = { 1, 2, 3, 4 };
+
+            // Set title.
+            this.ingredients.Titles.Add("Ingredients");
+
+            // Add series.
+            for (int i = 0; i < seriesArray.Length; i++)
+            {
+                // Add series.
+                Series series = this.ingredients.Series.Add(seriesArray[i]);
+
+                // Add point.
+                series.Points.Add(pointsArray[i]);
+            }
         }
     }
 }
