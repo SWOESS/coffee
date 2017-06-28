23ac8a82d7f64828cebfaff57c9625e7d26d3b89
Shamaim Wöss <Shamaim Wss@SHAMAIM_WOESS>
2017-06-28 09:50:47 +0200

    commented chart creation and checked that funtionality works

--- Kaffeemaschine/Kaffeemaschine/VerwaltungsForm.cs
+++ Kaffeemaschine/Kaffeemaschine/VerwaltungsForm.cs
@@ -50,25 +50,33 @@ namespace Kaffeemaschine
 
         private void VerwaltungsForm_Load(object sender, EventArgs e)
         {
+            //Serie = Reihe von Datenpunkten, die in gewählter Formatierung in einer Chartarea angezeigt werden
+            //Serie für Datenanzeige wird erstellt, mit dem namen Füllstand
             Series series = chart1.Series.Add("Füllstand");
-            series.ChartType = SeriesChartType.Bar;
+            //Die Serie soll ein Balkendiagramm sein
+            series.ChartType = SeriesChartType.StackedBar;
+            //Die Serie wird zur im Designer erstellen ChartArea per Name hinzugefügt.
             series.ChartArea = "FillChartArea";
             // Data arrays.
+            //Arrays mit den Daten zu den Ingredients
             string[] seriesArray = Globals.getIngredientNames();
             int[] pointsArray = Globals.getIngredientIndexes();
             double[] fillArray = Globals.getIngredientFill();
+
             // Set title.
             this.chart1.Titles.Add("Füllstände");
             chart1.Titles[0].Text = "test";
             // Add series.
+            //Es wird durch das array geloopt und für jeden Ingredient wird ein Datenpunkt in die Serie eingefügt
             for (int i = 0; i < seriesArray.Length; i++)
             {
-                series.Points.AddXY(i, fillArray[i]);
-                /*
-                DataPoint p = new DataPoint(series);
+                DataPoint p = new DataPoint();
+                p.XValue = i;
+                p.YValues = fillArray;
                 p.Label = seriesArray[i];
-                p.SetValueXY(i, 4);
-                */        
+                //i ist quasi der Index des Ingredients, die X Achse im Diagramm (immer um eins höher)
+                //fillArray[i] ist der Y-Wert des Datenpunkts, Der Füllstand der Ingredients. Dies ist die Länge des Balkens.
+                series.Points.AddXY(i, fillArray[i]);        
             }
         }
     }
